85b8f56647ec70a7fe001880db0c22be0cbcf519
Bernát Kalló <kallo.bernat@gmail.com>
2016-11-11 15:31:17 +0000

    imported code, added test

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var it, spec, beforeEach
+exports.beforeEach = function () { beforeEach.apply(this, arguments) }
+exports.it = function () { it.apply(this, arguments) }
+exports.spec = function () { spec.apply(this, arguments) }
+
+var allSuitesDone = Promise.resolve()
+
+exports.describe = function describe (name, fn) {
+  return (allSuitesDone = allSuitesDone.then(function () {
+    console.log('\n----------- Testing ' + name)
+    if (!fn) return
+    var hooks = { beforeEach: [] }
+    exports.beforeEach = function beforeEach (fn) {
+      if (!fn) {
+        console.error('No fn in beforeEach')
+        return
+      }
+      hooks.beforeEach.push(fn)
+    }
+    var allCasesDone = Promise.resolve()
+    exports.spec = exports.it = function it (subName, fn) {
+      var doIt = function doIt () {
+        if (!fn) {
+          console.warn(name + ' ' + subName + ' pending')
+          return
+        }
+        var scope = {}
+
+        return sequentially(hooks.beforeEach.map(function (fn) {
+          return function () {
+            return fn.apply(scope, [])
+          }
+        })).then(function () {
+          return fn.apply(scope, [])
+        }).then(function () {
+          console.log(name + ' ' + subName + ' ok')
+        }, function (err) {
+          console.error(name + ' ' + subName + ' failed')
+          console.error(err)
+        })
+      }
+      allCasesDone = allCasesDone.then(doIt)
+    }
+    try {
+      fn()
+      return allCasesDone
+    } catch (err) {
+      console.error(err)
+    }
+  }))
+}
+
+function sequentially (fns) {
+  return fns.reduce(function (p, fn) {
+    return p.then(fn)
+  }, Promise.resolve())
+}
---
+++ test.js
@@ -0,0 +1,160 @@
+/* global describe it beforeEach */
+var TM = require('./index.js')
+var expect = require('expect')
+var JsMockito = require('jsmockito').JsMockito
+var spy = JsMockito.spy
+var verify = JsMockito.verify
+
+var consoleLog = console.log
+var consoleError = console.error
+
+describe('describe', () => {
+  beforeEach(() => {
+    console.log = spy(consoleLog)
+    console.error = spy(consoleError)
+  })
+
+  it("outputs ----- Testing <name>'", () =>
+    TM.describe('hello', () => {
+
+    })
+    .then(() => {
+      verify(console.log)('\n----------- Testing hello')
+    })
+  )
+})
+
+describe('it', () => {
+  beforeEach(() => {
+    console.log = spy(consoleLog)
+    console.error = spy(consoleError)
+  })
+
+  it('executes the code', () =>
+    TM.describe('hello', () => {
+      TM.it('is said', () => {
+        console.log('hello')
+      })
+    })
+    .then(() => {
+      verify(console.log)('hello')
+    })
+  )
+
+  var demoError = Error('demo error')
+
+  it('outputs success', () =>
+    TM.describe('hello', () => {
+      TM.it('is said', () => {
+        console.log('hello')
+      })
+    })
+    .then(() => {
+      verify(console.log)('hello is said ok')
+    })
+  )
+
+  it('outputs failures with error objects', () =>
+    TM.describe('hello', () => {
+      TM.it('is messed up', () => {
+        throw demoError
+      })
+    })
+    .then(() => {
+      verify(console.error)('hello is messed up failed')
+      verify(console.error)(demoError)
+    })
+  )
+
+  it('can run two test cases with first one failing', () =>
+    TM.describe('hello', () => {
+      TM.it('is messed up', () => {
+        throw demoError
+      })
+      TM.it('is said', () => {
+        console.log('hello')
+      })
+    })
+    .then(() => {
+      verify(console.error)('hello is messed up failed')
+      verify(console.error)(demoError)
+      verify(console.log)('hello')
+      verify(console.log)('hello is said ok')
+    })
+  )
+
+  it('can be called spec', () =>
+    TM.describe('hello', () => {
+      TM.spec('is messed up', () => {
+        throw demoError
+      })
+      TM.spec('is said', () => {
+        console.log('hello')
+      })
+    })
+    .then(() => {
+      verify(console.error)('hello is messed up failed')
+      verify(console.error)(demoError)
+      verify(console.log)('hello')
+      verify(console.log)('hello is said ok')
+    })
+  )
+})
+
+describe('beforeEach', () => {
+  var c
+  it('is run before each test case', () =>
+    TM.describe('counter', () => {
+      c = 1
+      TM.beforeEach(() => {
+        c *= 10
+      })
+      TM.it('adds 3', () => {
+        c += 3
+      })
+      TM.it('adds 5', () => {
+        c += 5
+      })
+      TM.it('adds 7', () => {
+        c += 7
+      })
+      TM.it('results in 13570', () => {
+        expect(c).toBe(13570)
+      })
+    })
+    .then(() => {
+      verify(console.log)('counter results in 13570 ok')
+    })
+  )
+
+  it('can pass values to test cases', () => {
+    TM.describe('hello', () => {
+      TM.beforeEach(() => {
+        this.c = 5
+      })
+      TM.it('works', () => {
+        console.log(this.c)
+      })
+    })
+    .then(() => {
+      verify(console.log)(5)
+    })
+  })
+
+  it('can pass values to next beforeEach handlers', () => {
+    TM.describe('hello', () => {
+      TM.beforeEach(() => {
+        this.c = 5
+      })
+      TM.beforeEach(() => {
+        console.log(this.c)
+      })
+      TM.it('works', () => {
+      })
+    })
+    .then(() => {
+      verify(console.log)(5)
+    })
+  })
+})
+
